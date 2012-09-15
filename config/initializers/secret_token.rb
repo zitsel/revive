aaa650eef74bd299deb6df44f2cc0e89302c3abf
Sterling James <sterling@revive-clothiers.com>
2012-09-15 03:25:46 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Revive::Application.config.secret_token = '817dc855bb1b799bfd17bcf8b1995d8cd32c0a26743040fe5af64132cbbe60dcc52ed0626b780cc3453aca9c14ed83356ef4b1c0ebcf9d2ae5edc03673373d46'
